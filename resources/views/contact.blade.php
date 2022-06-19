49efc818b84860027b83758ba033116c48aa61f8
joaovictor1997 <victorcrf@live.com>
2022-06-19 10:23:12 -0300

    teste

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>página de contato</h1>
+<a href="{{route('welcome')}}">Voltar</a>
---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.main')
+
+@section('title', 'Criar Evento')
+
+@section('content')
+
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" id="title" placeholder="Nome do evento" name="title" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="city">Cidade:</label>
+            <input type="text" id="city" placeholder="Local do evento" name="city" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="private">O evento é privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="description">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que acontecerá no evento?" ></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar Evento">
+    </form>
+</div>
+
+@endsection
